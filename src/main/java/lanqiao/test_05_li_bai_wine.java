b6db75ed8444e0cba940b44c2a77edb43a4c67d7
tangchengliang <chengliang.tang@dbappsecurity.com.cn>
2022-04-07 16:56:10 +0800

    2013 年真题

--- src/main/java/lanqiao/test_05_li_bai_wine.java
+++ src/main/java/lanqiao/test_05_li_bai_wine.java
@@ -3,19 +3,20 @@ package src.main.java.lanqiao;
 public class test_05_li_bai_wine {
     static int count=0;
     public static void main(String[] args) {
-        f(5,9,2);
+        dfs(5,9,2);
         System.out.println(count);
     }
 
-    private static void f(int a, int b, int sum) {
+    // 递归
+    private static void dfs(int a, int b, int sum) {
         if(a==0 && b==0 && sum==1){
             count++;
         }
         if(a>0){
-            f(a-1,b,sum*2);
+            dfs(a-1,b,sum*2);
         }
         if(b>0){
-            f(a,b-1,sum-1);
+            dfs(a,b-1,sum-1);
         }
     }
 }
---
+++ src/main/java/lanqiao/test_06_Number_card_types.java
@@ -0,0 +1,7 @@
+package src.main.java.lanqiao;
+
+public class test_06_Number_card_types {
+    public static void main(String[] args) {
+
+    }
+}
